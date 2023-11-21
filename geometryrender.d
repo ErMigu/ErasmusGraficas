71bb2b7afbbc1fdae0e94c84f09a9356f8c61ac8
ErMigu <miguelrivas2003@gmail.com>
2023-11-21 10:11:07 +0100

    PARTE 2 | COMMIT 1

---
+++ geometryrender.d
@@ -0,0 +1,5 @@
+geometryrender.o: geometryrender.cpp geometryrender.h openglwindow.h \
+ lib/ImGui/imgui.h lib/ImGui/imconfig.h \
+ lib/ImGuiFileDialog/ImGuiFileDialog.h \
+ lib/ImGuiFileDialog/ImGuiFileDialogConfig.h lib/ImGui/imgui_impl_glfw.h \
+ lib/ImGui/imgui_impl_opengl3.h 3dstudio.h matrixRoutinesAndOBJ.h
